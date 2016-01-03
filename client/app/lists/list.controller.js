535b162587a006d32a322482c57d75dbd8af6ce9
Jan Clarin <jan@clarin.me>
2016-01-02 23:49:40 -0700

    add list create/update/delete functions

--- client/app/lists/list.controller.js
+++ client/app/lists/list.controller.js
@@ -13,6 +13,13 @@
     function ListController($state, $stateParams, $uibModal, listService) {
         var vm = this;
 
+        vm.createListModalInstance = createListModalInstance;
+        vm.createListModal = createListModal;
+        vm.editListModal = editListModal;
+        vm.deleteListModal = deleteListModal;
+        vm.saveList = saveList;
+        vm.editList = editList;
+        vm.deleteList = deleteList;
         vm.viewList = viewList;
         vm.userId = $stateParams.userId; // Get user's ID from state params.
         vm.selectedListId = null;
@@ -25,8 +32,10 @@
             listService.query(vm.userId)
                 .then(function(lists) {
                     vm.lists = lists;
-                    var list = vm.lists[0];
-                    vm.selectedListId = list.id;
+                    if (!vm.selectedListId) {
+                        var list = vm.lists[0];
+                        vm.selectedListId = list.id; // Set selected list by default.
+                    }
                     $state.go('lists.detail', { listId: list.id });
                 });
         }
@@ -41,16 +50,152 @@
         }
 
         /**
-         * TODO.
-         * Opens an instance of the list modal.
+         * Opens an modal instance and returns its reference.
+         * @param isAnimationEnabled Boolean for modal opening animation.
+         * @param modalContents Object containing text fields for the modal.
+         * @param list The list object to apply changes to. Can be an empty object (new list).
+         * @returns {*} Modal instance.
          */
-        function createListModal() {
-            var modalInstance = $uibModal.open({
-                animation: true,
+        function createListModalInstance(isAnimationEnabled, modalContents, list) {
+            return $uibModal.open({
+                animation: isAnimationEnabled,
                 templateUrl: 'app/lists/list-modal.html',
                 controller: 'ListModalController',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                resolve: {
+                    modalContents: function () {
+                        return modalContents;
+                    },
+                    list: function () {
+                        return list;
+                    }
+                }
             });
+        }
+
+        /**
+         * Displays a modal for creating a new list.
+         */
+        function createListModal() {
+            var modalContents = {
+                title: 'Add list',
+                confirmText: 'Confirm'
+            };
+            var modalInstance = createListModalInstance(true, modalContents, {});
+
+            modalInstance.result
+                .then(function (list) {
+                    return vm.saveList(list, vm.userId);
+                })
+                .catch(function (err) {
+                    // TODO: Display error.
+                });
+        }
+
+        /**
+         * Displays a modal for editing a list.
+         * @param list The list to edit.
+         */
+        function editListModal(list) {
+            var modalContents = {
+                title: 'Edit list',
+                confirmText: 'Update'
+            };
+            var modalInstance = createListModalInstance(
+                true,
+                modalContents,
+                angular.copy(list) // Copy so changes aren't reflected in the list.
+            );
+
+            modalInstance.result
+                .then(function (updatedList) {
+                    return vm.editList(updatedList);
+                })
+                .catch(function (err) {
+                    // TODO: Display error.
+                });
+
+        }
+
+        /**
+         * Displays a confirmation modal for deleting a list.
+         * @param list
+         */
+        function deleteListModal(list) {
+            var modalInstance = $uibModal.open({
+                animation: true,
+                size: 'sm',
+                templateUrl: 'app/components/confirmation-modal.html',
+                controller: 'ConfirmationModalController',
+                controllerAs: 'vm',
+                resolve: {
+                    modalContents: function () {
+                        // The modal fields.
+                        return {
+                            title: 'Are you sure?',
+                            body: 'This list will be permanently deleted.',
+                            confirmText: 'Delete'
+                        };
+                    }
+                }
+            });
+
+            modalInstance.result
+                .then(function () {
+                    return vm.deleteList(list);
+                })
+                .catch(function (err) {
+                    // TODO: Handle error when deleting a list.
+                });
+        }
+
+        /**
+         * Saves a list in the database.
+         * @param list The list to save.
+         * @param userId The ID of the user who owns the list.
+         * @returns {*} A promise.
+         */
+        function saveList(list, userId) {
+            return listService.save(list, userId)
+                .then(function (createdList) {
+                    vm.lists.push(createdList); // Add the new list to the list of lists.
+                });
+        }
+
+        /**
+         * Updates a list in the database and updates it in in the local list.
+         * @param list The list with new information.
+         * @returns {*} A promise.
+         */
+        function editList(list) {
+            return listService.update(list)
+                .then(function () {
+                    // Find the list and replace it in the list.
+                    for (var i = 0; i < vm.lists.length; i++) {
+                        if (vm.lists[i].id === list.id) {
+                            vm.lists[i] = list; // Replace the old list.
+                            break;
+                        }
+                    }
+                });
+        }
+
+        /**
+         * Deletes a list in the database and removes it from the local list.
+         * @param list The list to be deleted.
+         * @returns {*} A promise.
+         */
+        function deleteList(list) {
+            return listService.remove(list.id)
+                .then(function () {
+                    // Find the list and remove it from the list.
+                    for (var i = 0; i < vm.lists.length; i++) {
+                        if (vm.lists[i].id === list.id) {
+                            vm.lists.splice(i, 1); // Remove the list locally.
+                            break;
+                        }
+                    }
+                });
         }
     }
 })();
