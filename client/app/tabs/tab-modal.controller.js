de68d5a6841414e5844c77aef04f98c9bed0fecf
Jan Clarin <hi@janclarin.com>
2016-03-06 17:07:02 -0700

    add upload progress bar to tab modal

--- client/app/tabs/tab-modal.controller.js
+++ client/app/tabs/tab-modal.controller.js
@@ -8,9 +8,9 @@
         .module('app')
         .controller('TabModalController', TabModalController);
 
-    TabModalController.$inject = ['$uibModalInstance', 'modalContents', 'tab', 'tabProgresses', 'tabTypes', 'awsService'];
+    TabModalController.$inject = ['$uibModalInstance', '$q', 'modalContents', 'tab', 'tabProgresses', 'tabTypes', 'awsService'];
 
-    function TabModalController($uibModalInstance, modalContents, tab, tabProgresses, tabTypes, awsService) {
+    function TabModalController($uibModalInstance, $q, modalContents, tab, tabProgresses, tabTypes, awsService) {
         var vm = this;
 
         vm.upload = upload;
@@ -23,6 +23,8 @@
         vm.selectedType = (tab.typeId) ? tabTypes[tab.typeId - 1] : tabTypes[0];
         vm.selectedProgress = (tab.progressId) ? tabProgresses[tab.progressId - 1] : tabProgresses[0];
         vm.sourceFile = null;
+        vm.uploadProgress = null;
+        vm.uploadStatus = 'success';
 
         /**
          * Uploads a file to AWS S3 and sets tab's source field.
@@ -33,6 +35,10 @@
                 awsService.upload(file)
                     .then(function (response) {
                         vm.tab.source = response.config.url.split('?')[0]; // Remove query params.
+                    }, function (error) {
+                        vm.uploadStatus = 'danger';
+                    }, function (progress) {
+                        vm.uploadProgress = parseInt(100.0 * progress.loaded / progress.total);
                     });
             }
         }
@@ -44,6 +50,7 @@
         }
 
         function cancel() {
+            // TODO: Abort upload?
             $uibModalInstance.dismiss('cancel');
         }
     }
