ec146ae896005a21981ca16d357b0eafadedc1a7
Jan Clarin <hi@janclarin.com>
2016-03-21 22:33:51 -0600

    change tabprogress/tabtype name format

--- server/seeders/20151220192358-inittabtables-seeder.js
+++ server/seeders/20151220192358-inittabtables-seeder.js
@@ -5,14 +5,14 @@ module.exports = {
         var currentDate = new Date();
         return [
             queryInterface.bulkInsert('TabProgress', [
-                { name: 'Currently learning', createdAt: currentDate, updatedAt: currentDate },
+                { name: 'Currently Learning', createdAt: currentDate, updatedAt: currentDate },
                 { name: 'Learned', createdAt: currentDate, updatedAt: currentDate },
-                { name: 'Plan to learn', createdAt: currentDate, updatedAt: currentDate }
+                { name: 'Plan to Learn', createdAt: currentDate, updatedAt: currentDate }
             ], {}),
             queryInterface.bulkInsert('TabType', [
                 { name: 'Tab', createdAt: currentDate, updatedAt: currentDate },
                 { name: 'Chords', createdAt: currentDate, updatedAt: currentDate },
-                { name: 'Sheet', createdAt: currentDate, updatedAt: currentDate },
+                { name: 'Music Sheet', createdAt: currentDate, updatedAt: currentDate },
                 { name: 'Guitar Pro', createdAt: currentDate, updatedAt: currentDate }
             ], {})
         ];
